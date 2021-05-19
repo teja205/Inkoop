f2568fef0c82a4dd0c844a5833f3ce94ccf7d63d
teja205 <73633131+teja205@users.noreply.github.com>
2021-05-20 01:08:34 +0530

    Add files via upload

---
+++ inkoop/inkoop.js
@@ -0,0 +1,10 @@
+var menulist = document.getElementById("menulist");
+
+
+function toggleMenu(){
+    if(menulist.style.display == "none"){
+        menulist.style.display = "block";
+    }else{
+        menulist.style.display="none";
+    }
+}
